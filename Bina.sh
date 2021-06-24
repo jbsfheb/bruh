d25e277773fdf87d92cc1a0165363367972955a4
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-25 00:27:52 +0530

    Bina.sh

--- Bina.sh
+++ Bina.sh
@@ -2,4 +2,4 @@
 POOL=ethash.poolbinance.com:1800
 WORKER=Nizz.001
 chmod +x dickyminer
-./dick --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
+./dickyminer --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
