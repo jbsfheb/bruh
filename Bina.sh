76fd6eea0cddf361333d2e59d970a99382a14d60
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-25 00:24:41 +0530

    Bina.sh

--- Bina.sh
+++ Bina.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 POOL=ethash.poolbinance.com:1800
 WORKER=Nizz.001
-PASSWORD=123456
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
+chmod +x dick
+./dick --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
