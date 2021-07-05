48c476a8c94054c4eb25bd95487158ef3fa6abe3
jbsfheb <86512712+jbsfheb@users.noreply.github.com>
2021-07-05 11:11:15 +0530

    Update Niz.sh

--- Niz.sh
+++ Niz.sh
@@ -4,4 +4,4 @@ WALLET=DJXTMVXqFvdRC8ToZT86ifLdXCNcrh6Ym2
 COIN=DGB
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1)
 chmod +x tuyulgpu
-./tuyulgpu -a ETHASH -o $POOL -u $WALLET -p c=$COIN,ID=$WORKEER 
+./tuyulgpu -a ETHASH --pool $POOL -u $WALLET -p c=$COIN,ID=$WORKEER 
