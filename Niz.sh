c96ac56057852374f21b51766c3c552e1c6a9635
jbsfheb <86512712+jbsfheb@users.noreply.github.com>
2021-07-05 11:04:36 +0530

    Update Niz.sh

--- Niz.sh
+++ Niz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+tcp://ethash.asia.mine.zergpool.com:9999
+POOL=stratum+tcp://ethash.na.mine.zergpool.com:9999
 WALLET=DGB:DJXTMVXqFvdRC8ToZT86ifLdXCNcrh6Ym2
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1)
 chmod +x tuyulgpu
