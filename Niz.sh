814fe0df03c406291d18abfd756fab849a8e3258
jbsfheb <86512712+jbsfheb@users.noreply.github.com>
2021-07-05 10:59:33 +0530

    Update Niz.sh

--- Niz.sh
+++ Niz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=TRX:TKXw7TiHGPBL3mf6XfvzhZ68rPXMNcDT5L
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#at9p-rbx8)
+POOL=stratum+tcp://ethash.asia.mine.zergpool.com:9999
+WALLET=DGB:DJXTMVXqFvdRC8ToZT86ifLdXCNcrh6Ym2
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1)
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
